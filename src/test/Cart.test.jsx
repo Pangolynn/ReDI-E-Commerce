efed778ffae64bba51f44d04b5ac8a6fb15b0415
Samantha S <sami.berlynn@proton.me>
2025-05-29 18:03:59 +0200

    Added additional testing packages. Added tests for Product and ProductDetail.

---
+++ src/test/Cart.test.jsx
@@ -0,0 +1,12 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+describe("Cart", () => {
+    it("should render an empty message when there's nothing in the cart", () => {
+        // render(<Cart />);
+        //
+        // expect(
+        //     screen(getByText("Cart is Empty. Add some products to your cart!")),
+        // ).toBeInTheDocument();
+    });
+});
